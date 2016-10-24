52fb1a5a480e744e6697a7ca5f5c61e5d6f1b5f5
elkhanaliyev <elchan.alijev@gmail.com>
2016-10-24 10:17:29 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,27 @@
 require(plyr)
 
-# Directories and files
-uci_hard_dir <- "UCI\ HAR\ Dataset"
-feature_file <- paste(uci_hard_dir, "/features.txt", sep = "")
-activity_labels_file <- paste(uci_hard_dir, "/activity_labels.txt", sep = "")
-x_train_file <- paste(uci_hard_dir, "/train/X_train.txt", sep = "")
-y_train_file <- paste(uci_hard_dir, "/train/y_train.txt", sep = "")
-subject_train_file <- paste(uci_hard_dir, "/train/subject_train.txt", sep = "")
-x_test_file  <- paste(uci_hard_dir, "/test/X_test.txt", sep = "")
-y_test_file  <- paste(uci_hard_dir, "/test/y_test.txt", sep = "")
-subject_test_file <- paste(uci_hard_dir, "/test/subject_test.txt", sep = "")
+# Defines directories and files from raw data
+uci_har <- "UCI\ HAR\ Dataset"
+feature_file <- paste(uci_har, "/features.txt", sep = "")
+labels_f <- paste(uci_har, "/activity_labels.txt", sep = "")
+train_f<- paste(uci_har, "/train/X_train.txt", sep = "")
+y_train_f <- paste(uci_har, "/train/y_train.txt", sep = "")
+subject_train_file <- paste(uci_har, "/train/subject_train.txt", sep = "")
+x_test_file  <- paste(uci_har, "/test/X_test.txt", sep = "")
+y_test_file  <- paste(uci_har, "/test/y_test.txt", sep = "")
+subject_test_file <- paste(uci_har, "/test/subject_test.txt", sep = "")
 
-# Load raw data
+# Loads and assigsn raw data
 features <- read.table(feature_file, colClasses = c("character"))
-activity_labels <- read.table(activity_labels_file, col.names = c("ActivityId", "Activity"))
+activity_labels <- read.table(labels_f, col.names = c("ActivityId", "Activity"))
 x_train <- read.table(x_train_file)
-y_train <- read.table(y_train_file)
+y_train <- read.table(y_train_f)
 subject_train <- read.table(subject_train_file)
 x_test <- read.table(x_test_file)
 y_test <- read.table(y_test_file)
 subject_test <- read.table(subject_test_file)
-
-##################################################################
-# 1. Merges the training and the test sets to create one data set.
-##################################################################
-
-# Binding sensor data
+# 1. Merges training and test sets to one data set.
+# Binding test sensor and sensor  data
 training_sensor_data <- cbind(cbind(x_train, subject_train), y_train)
 test_sensor_data <- cbind(cbind(x_test, subject_test), y_test)
 sensor_data <- rbind(training_sensor_data, test_sensor_data)
@@ -34,22 +30,15 @@ sensor_data <- rbind(training_sensor_data, test_sensor_data)
 sensor_labels <- rbind(rbind(features, c(562, "Subject")), c(563, "ActivityId"))[,2]
 names(sensor_data) <- sensor_labels
 
-############################################################################################
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-############################################################################################
+#2. Extracts measurements on the mean and standard deviation for each measurement.
 
 sensor_data_mean_std <- sensor_data[,grepl("mean|std|Subject|ActivityId", names(sensor_data))]
-
-###########################################################################
 # 3. Uses descriptive activity names to name the activities in the data set
-###########################################################################
 
 sensor_data_mean_std <- join(sensor_data_mean_std, activity_labels, by = "ActivityId", match = "first")
 sensor_data_mean_std <- sensor_data_mean_std[,-1]
 
-##############################################################
 # 4. Appropriately labels the data set with descriptive names.
-##############################################################
 
 # Remove parentheses
 names(sensor_data_mean_std) <- gsub('\\(|\\)',"",names(sensor_data_mean_std), perl = TRUE)
@@ -67,9 +56,8 @@ names(sensor_data_mean_std) <- gsub('\\.std',".StandardDeviation",names(sensor_d
 names(sensor_data_mean_std) <- gsub('Freq\\.',"Frequency.",names(sensor_data_mean_std))
 names(sensor_data_mean_std) <- gsub('Freq$',"Frequency",names(sensor_data_mean_std))
 
-######################################################################################################################
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-######################################################################################################################
+# 5. Creates the final  tidy data set with the average of each variable for each activity and each subject.
+
 
 sensor_avg_by_act_sub = ddply(sensor_data_mean_std, c("Subject","Activity"), numcolwise(mean))
 write.table(sensor_avg_by_act_sub, file = "sensor_avg_by_act_sub.txt")
