eaa0a4dfb29517e8d6242333bf43ab5fde7fd265
elkhanaliyev <elchan.alijev@gmail.com>
2016-10-24 10:19:59 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -20,7 +20,7 @@ subject_train <- read.table(subject_train_file)
 x_test <- read.table(x_test_file)
 y_test <- read.table(y_test_file)
 subject_test <- read.table(subject_test_file)
-# 1. Merges training and test sets to one data set.
+
 # Binding test sensor and sensor  data
 training_sensor_data <- cbind(cbind(x_train, subject_train), y_train)
 test_sensor_data <- cbind(cbind(x_test, subject_test), y_test)
@@ -30,21 +30,21 @@ sensor_data <- rbind(training_sensor_data, test_sensor_data)
 sensor_labels <- rbind(rbind(features, c(562, "Subject")), c(563, "ActivityId"))[,2]
 names(sensor_data) <- sensor_labels
 
-#2. Extracts measurements on the mean and standard deviation for each measurement.
+#Extracts measurements on the mean and standard deviation for each measurement.
 
 sensor_data_mean_std <- sensor_data[,grepl("mean|std|Subject|ActivityId", names(sensor_data))]
-# 3. Uses descriptive activity names to name the activities in the data set
+# Uses descriptive activity names to name the activities in the data set
 
 sensor_data_mean_std <- join(sensor_data_mean_std, activity_labels, by = "ActivityId", match = "first")
 sensor_data_mean_std <- sensor_data_mean_std[,-1]
 
-# 4. Appropriately labels the data set with descriptive names.
+# Subs columns, lables and etc
 
-# Remove parentheses
+# Subs parentheses
 names(sensor_data_mean_std) <- gsub('\\(|\\)',"",names(sensor_data_mean_std), perl = TRUE)
 # Make syntactically valid names
 names(sensor_data_mean_std) <- make.names(names(sensor_data_mean_std))
-# Make clearer names
+# Subs names to readable tidy format
 names(sensor_data_mean_std) <- gsub('Acc',"Acceleration",names(sensor_data_mean_std))
 names(sensor_data_mean_std) <- gsub('GyroJerk',"AngularAcceleration",names(sensor_data_mean_std))
 names(sensor_data_mean_std) <- gsub('Gyro',"AngularSpeed",names(sensor_data_mean_std))
@@ -56,7 +56,7 @@ names(sensor_data_mean_std) <- gsub('\\.std',".StandardDeviation",names(sensor_d
 names(sensor_data_mean_std) <- gsub('Freq\\.',"Frequency.",names(sensor_data_mean_std))
 names(sensor_data_mean_std) <- gsub('Freq$',"Frequency",names(sensor_data_mean_std))
 
-# 5. Creates the final  tidy data set with the average of each variable for each activity and each subject.
+#Creates the final  tidy data set with the average of each variable for each activity and each subject.
 
 
 sensor_avg_by_act_sub = ddply(sensor_data_mean_std, c("Subject","Activity"), numcolwise(mean))
